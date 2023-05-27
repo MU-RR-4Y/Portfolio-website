89f3c41f2717d812c5ef513c31422bc42349f601
MU-RR-4Y <mmurray24@hotmail.com>
2023-05-27 16:58:14 +0100

    nav bar added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Navbar from '@/components/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-    hello
-    </>
+    <div>
+      <Navbar/>
+      <div>
+          {/* <h1>Michael Murray | Junior Developer</h1> */}
+      </div>
+    </div>
   )
 }
