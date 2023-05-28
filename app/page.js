07371b768f4fd4cf3f38aa93d110ad4a2bad9d98
MU-RR-4Y <mmurray24@hotmail.com>
2023-05-28 16:39:19 +0100

    initial contct section added

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
 import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
-
-
 import Image from 'next/image'
+import Contact from '@/components/Contact'
 
 export default function Home() {
   return (
@@ -15,6 +14,7 @@ export default function Home() {
       <About/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   )
 }
